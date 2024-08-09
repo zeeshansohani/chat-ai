9f7380f751eeaa15126981d54a7c61218ae04503
Sahil Sohani <sahilsohani@Sahils-MacBook-Air.local>
2024-08-09 05:27:45 +0500

    Update CORS origin URL

--- dist/app.js
+++ dist/app.js
@@ -1,17 +1,17 @@
-import express from 'express';
-import { config } from 'dotenv';
-import morgan from 'morgan';
-import appRouter from './routes/index.js';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
+import express from "express";
+import { config } from "dotenv";
+import morgan from "morgan";
+import appRouter from "./routes/index.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 config();
 const app = express();
 // middlewares
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
+    origin: "https://keen-duckanoo-eb6256.netlify.app",
+    credentials: true,
 }));
 // remove it in production
 app.use(morgan("dev"));
--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,25 @@
-import express from 'express'
-import {config} from 'dotenv'
-import morgan from 'morgan'
-import appRouter from './routes/index.js';
-import cookieParser from 'cookie-parser';
-import cors from 'cors'
+import express from "express";
+import { config } from "dotenv";
+import morgan from "morgan";
+import appRouter from "./routes/index.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 config();
 const app = express();
 
 // middlewares
-app.use(express.json())
-app.use(cookieParser(process.env.COOKIE_SECRET))
-app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
-}))
+app.use(express.json());
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+  cors({
+    origin: "https://keen-duckanoo-eb6256.netlify.app",
+    credentials: true,
+  })
+);
 
 // remove it in production
-app.use(morgan("dev"))
+app.use(morgan("dev"));
 
-app.use("/api/v1", appRouter)
+app.use("/api/v1", appRouter);
 
 export default app;
